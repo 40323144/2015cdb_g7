9b0e1974a706ff8094a965a89611c89caf04d633
40223236 <40223236@gm.nfu.edu.tw>
2015-05-04 17:00:36 +0800

    first

--- wsgi.py
+++ wsgi.py
@@ -105,11 +105,6 @@ class Hello(object):
     <!-- 啟動 brython() -->
     <body onload="brython()">
         
-    <form method=POST action=doCheck>
-    請輸入您所猜的整數:<input type=text name=guess><br />
-    <input type=submit value=send>
-    </form>
-    <hr>
     <!-- 以下在網頁內嵌 Brython 程式 -->
     <script type="text/python">
     from browser import document, alert
@@ -122,49 +117,74 @@ class Hello(object):
     </script>
     <input id="zone"><button id="mybutton">click !</button>
     <hr>
+
+     <form method=POST action=index>
+        齒數1:<input type=text name=N1><br />
+        齒數2:<input type=text name=N2><br />
+        模數:<input type=text name=M><br />
+        壓力角:<input type=text name=P><br />
+        <input type=submit value=send>
+
+        <input type=submit value=send>
+
+        </form>
+        <hr>
+
     <!-- 以下為 canvas 畫圖程式 -->
     <script type="text/python">
     # 從 browser 導入 document
     from browser import document
-    import math
+    from math import *
+    # 請注意, 這裡導入位於 Lib/site-packages 目錄下的 spur.py 檔案
+    import spur
 
-    # 畫布指定在名稱為 plotarea 的 canvas 上
-    # 以下使用中文變數名稱
+    # 準備在 id="plotarea" 的 canvas 中繪圖
     canvas = document["plotarea"]
     ctx = canvas.getContext("2d")
 
-    # 用紅色畫一條直線
-    ctx.beginPath()
-    ctx.lineWidth = 3
-    ctx.moveTo(0, 0)
-    ctx.lineTo(0, 500)
-    ctx.strokeStyle = "red"
-    ctx.stroke()
+    # 以下利用 spur.py 程式進行繪圖, 接下來的協同設計運算必須要配合使用者的需求進行設計運算與繪圖
+    # 其中並將工作分配給其他組員建立類似 spur.py 的相關零件繪圖模組
+    # midx, midy 為齒輪圓心座標, rp 為節圓半徑, n 為齒數, pa 為壓力角, color 為線的顏色
+    # Gear(midx, midy, rp, n=20, pa=20, color="black"):
+    # 模數決定齒的尺寸大小, 囓合齒輪組必須有相同的模數與壓力角
+    # 壓力角 pa 單位為角度
+    pa = P
+    # m 為模數
+    m = M
+    # 第1齒輪齒數
+    n_g1 = N1
+    # 第2齒輪齒數
+    n_g2 = N2
 
-    # 用藍色再畫一條直線
-    ctx.beginPath()
-    ctx.lineWidth = 3
-    ctx.moveTo(0, 0)
-    ctx.lineTo(500, 0)
-    ctx.strokeStyle = "blue"
-    ctx.stroke()
+    # 計算兩齒輪的節圓半徑
+    rp_g1 = m*n_g1/2
+    rp_g2 = m*n_g2/2
 
-    # 用綠色再畫一條直線
-    ctx.beginPath()
-    ctx.lineWidth = 3
-    ctx.moveTo(0, 0)
-    ctx.lineTo(500, 500)
-    ctx.strokeStyle = "green"
-    ctx.stroke()
+    # 將第1齒輪順時鐘轉 90 度
+    # 使用 ctx.save() 與 ctx.restore() 以確保各齒輪以相對座標進行旋轉繪圖
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(400,400)
+    # rotate to engage
+    ctx.rotate(pi/2)
+    # put it back
+    ctx.translate(-400,-400)
+    spur.Spur(ctx).Gear(400,400,rp_g1,n_g1, pa, "blue")
+    ctx.restore()
 
-    # 用黑色畫一個圓
-    ctx.beginPath()
-    ctx.lineWidth = 3
-    ctx.strokeStyle = "black"
-    ctx.arc(250,250,50,0,2*math.pi)
-    ctx.stroke()
+    # 將第2齒輪逆時鐘轉 90 度之後, 再多轉一齒, 以便與第1齒輪進行囓合
+    ctx.save()
+    # translate to the origin of second gear
+    ctx.translate(400+rp_g1+rp_g2,400)
+    # rotate to engage
+    ctx.rotate(-pi/2-pi/n_g2)
+    # put it back
+    ctx.translate(-(400+rp_g1+rp_g2),-400)
+    spur.Spur(ctx).Gear(400+rp_g1+rp_g2,400,rp_g2,n_g2, pa, "black")
+    ctx.restore()
+
     </script>
-    <canvas id="plotarea" width="800" height="600"></canvas>
+    <canvas id="plotarea" width="1200" height="1200"></canvas>
     </body>
     </html>
     '''
@@ -547,21 +567,10 @@ class Hello(object):
     n_g1 = 13
     # 第2齒輪齒數
     n_g2 = 17
-    # 第3齒輪齒數
-    n_g3 = 11
-    # 第4齒輪齒數
-    n_g4 = 13
-    # 第5齒輪齒數
-    n_g5 = 17
-    # 第6齒輪齒數
-    n_g6 = 11
+
     # 計算兩齒輪的節圓半徑
     rp_g1 = m*n_g1/2
     rp_g2 = m*n_g2/2
-    rp_g3 = m*n_g3/2
-    rp_g4 = m*n_g4/2
-    rp_g5 = m*n_g5/2
-    rp_g6 = m*n_g6/2
 
     # 將第1齒輪順時鐘轉 90 度
     # 使用 ctx.save() 與 ctx.restore() 以確保各齒輪以相對座標進行旋轉繪圖
@@ -585,52 +594,6 @@ class Hello(object):
     ctx.translate(-(400+rp_g1+rp_g2),-400)
     spur.Spur(ctx).Gear(400+rp_g1+rp_g2,400,rp_g2,n_g2, pa, "black")
     ctx.restore()
-
-    # 第3齒
-    ctx.save()
-    # translate to the origin of second gear
-    ctx.translate(400+rp_g1+rp_g2+rp_g2+rp_g3,400)
-    # rotate to engage
-    ctx.rotate(-pi/2-pi/n_g3)
-    # put it back
-    ctx.translate(-(400+rp_g1+rp_g2+rp_g2+rp_g3),-400)
-    spur.Spur(ctx).Gear(400+rp_g1+rp_g2+rp_g2+rp_g3,400,rp_g3,n_g3, pa, "green")
-    ctx.restore()
-
-    #第4齒
-    ctx.save()
-    # translate to the origin of second gear
-    ctx.translate(400+rp_g1+2*rp_g2+2*rp_g3+rp_g4,400)
-    # rotate to engage
-    ctx.rotate(-pi/2-pi/n_g4)
-    # put it back
-    ctx.translate(-(400+rp_g1+2*rp_g2+2*rp_g3+rp_g4),-400)
-    spur.Spur(ctx).Gear(400+rp_g1+2*rp_g2+2*rp_g3+rp_g4,400,rp_g4,n_g4, pa, "orange")
-    ctx.restore()
-
-    #第5齒
-    ctx.save()
-    # translate to the origin of second gear
-    ctx.translate(400+rp_g1+2*rp_g2+2*rp_g3+2*rp_g4+rp_g5,400)
-    # rotate to engage
-    ctx.rotate(-pi/2-pi/n_g5)
-    # put it back
-    ctx.translate(-(400+rp_g1+2*rp_g2+2*rp_g3+2*rp_g4+rp_g5),-400)
-    spur.Spur(ctx).Gear(400+rp_g1+2*rp_g2+2*rp_g3+2*rp_g4+rp_g5,400,rp_g5,n_g5, pa, "red")
-    ctx.restore()
-
-    #第6齒
-    ctx.save()
-    # translate to the origin of second gear
-    ctx.translate(400+rp_g1+2*rp_g2+2*rp_g3+2*rp_g4+2*rp_g5+rp_g6,400)
-    # rotate to engage
-    ctx.rotate(-pi/2-pi/n_g6)
-    # put it back
-    ctx.translate(-(400+rp_g1+2*rp_g2+2*rp_g3+2*rp_g4+2*rp_g5+rp_g6),-400)
-    spur.Spur(ctx).Gear(400+rp_g1+2*rp_g2+2*rp_g3+2*rp_g4+2*rp_g5+rp_g6,400,rp_g6,n_g6, pa, "yellow")
-    ctx.restore()
-
-
 
     </script>
     <canvas id="plotarea" width="3600" height="1200"></canvas>
